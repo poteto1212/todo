596f6c0dc540a8932c7912ffedd4a49c9adac571
Your Name <you@example.com>
2021-03-31 03:56:42 +0000

    init

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import TodoView,Detail
+from .views import TodoView,Detail,TodoCreateView,TodoDelete,TodoUpdateView
 
 urlpatterns=[
     path('list/',TodoView.as_view(),name='list'),
-    path('detail/<int:pk>/',Detail.as_view(),name='detail')
+    path('detail/<int:pk>/',Detail.as_view(),name='detail'),
+    path('create/',TodoCreateView.as_view(),name='create'),
+    path('delete/<int:pk>/',TodoDelete.as_view(),name='delete'),
+    path('update/<int:pk>/',TodoUpdateView.as_view(),name='update')
+    
     ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import TodoModel
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
+from django.urls import reverse_lazy
 
 class TodoView(ListView):
     model=TodoModel
@@ -9,3 +10,20 @@ class TodoView(ListView):
 class Detail(DetailView):
     model=TodoModel
     template_name='detail.html'
+    
+class TodoCreateView(CreateView):#adminフォームに似た要領で作成
+    model=TodoModel
+    template_name='create.html'
+    fields=['title','content','pic']
+    success_url=reverse_lazy('list') #投稿後自動的にdetail.htmlに飛ぶ(urlのname引数で指定)
+    
+class TodoDelete(DeleteView):
+    model=TodoModel
+    template_name='delete.html'
+    success_url=reverse_lazy('list')
+    
+class TodoUpdateView(UpdateView):
+    model=TodoModel
+    template_name='update.html'
+    fields=['title','content','pic']
+    success_url=reverse_lazy('list')
