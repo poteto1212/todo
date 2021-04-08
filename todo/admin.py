578194b0fb1e7d120a8055111d9ca057d3f88ad0
Your Name <you@example.com>
2021-04-08 06:30:01 +0000

    init

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TodoModel,SubjectModel
+from .models import TodoModel,SubjectModel,Field
 
 class TodoAdmin(admin.ModelAdmin):
     list_display=('subject','title','content','pic')
@@ -9,3 +9,5 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display=('subjects',)
 
 admin.site.register(SubjectModel,SubjectAdmin)
+
+admin.site.register(Field)
--- todo/models.py
+++ todo/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
+#科目選択(1対ｎ)
 class SubjectModel(models.Model):
-    subjects=models.CharField(verbose_name="科目",max_length=25)
+    subjects=models.CharField(verbose_name="作用領域",max_length=25)
     
     def __str__(self):
         return self.subjects
     
     class Meta:
         verbose_name="科目カテゴリー"
+#タグ(n対n)
+class Field(models.Model):
+    fields=models.CharField(verbose_name="作用機序",max_length=50)
+    
+    def __str__(self):
+        return self.fields
 
+#メインデータ
 class TodoModel(models.Model):
     subject=models.ForeignKey('SubjectModel',on_delete=models.CASCADE)
     title=models.CharField(max_length=100)
@@ -19,3 +27,14 @@ class TodoModel(models.Model):
         on_delete=models.CASCADE
         )
 # Create your models here.
+    field=models.ManyToManyField('Field',verbose_name='分野')
+    relation=models.ManyToManyField('self',verbose_name='関連分野',blank=True,null=True)
+    created_at=models.DateField(verbose_name='最終更新日',auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+        
+    
+    class Meta:
+        verbose_name="医薬品データベース"
+        verbose_name_plural="医薬品データベース"
